05befa12c8cf9420dbd153f3d4d52a6cbe98f980
trevor nkuna <trevornkuna7@gmail.com>
2023-04-14 10:35:03 +0200

    first commit

---
+++ function poundToKg(pounds: number ): num.ts
@@ -0,0 +1,35 @@
+function poundToKg(pounds: number ): number{
+
+    return pounds*0.453592;
+      
+     
+    }
+    console.log(poundToKg(10));
+    ``
+    
+    function inchToCm(inch: number): number{
+    
+     
+        return inch*2.54;
+    
+    }
+    console.log(inchToCm(10));
+    
+    
+    function fahrenheitToCelcius(Fahrenheit: number):number{
+    
+        return (Fahrenheit - 32) * 5/9;
+    
+    }
+    console.log(fahrenheitToCelcius(10));
+    
+    function feetToMeters(feet: number):number{
+      
+    
+        return feet*0.3048;
+      
+    }
+    console.log(feetToMeters(10));
+    
+    
+    
